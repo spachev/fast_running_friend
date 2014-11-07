6a9bc84e632c4b31b9611ac59e3f2c569925b327
Sasha Pachev <sasha@asksasha.com>
2014-11-06 20:19:59 -0700

    Sort workouts by date

--- jni/timer.c
+++ jni/timer.c
@@ -25,15 +25,15 @@ typedef struct st_run_list
 int run_timer_init(Run_timer* t, const char* file_prefix)
 {
   char* p;
-  
+
   bzero(t,sizeof(*t));
-  
+
   if (mem_pool_init(&t->mem_pool,RUN_TIMER_MEM_POOL_BLOCK))
     return 1;
-  
+
   if (!(t->file_prefix = strdup(file_prefix)))
     return 1;
-  
+
   if (!(p = strrchr(file_prefix,'/')))
     t->dir_len = 0;
   else
@@ -67,22 +67,22 @@ static uint print_time(char* buf, uint buf_size, ulonglong t)
   t_left -= ss_fract;
   ss_fract = (ss_fract / 100) + ((ss_fract % 100) > 50);
   if (ss_fract >= 10)
-  {  
+  {
     ss_fract -= 10;
     t_left += 1000;
-  }  
+  }
   ss = t_left / 1000;
   mm = ss / 60;
   ss = (ss % 60);
   hh = mm / 60;
   mm = (mm % 60);
-  
+
   if (hh)
   {
-    return snprintf(buf, buf_size, "%02d:%02d:%02d.%d", hh, mm, ss, ss_fract); 
+    return snprintf(buf, buf_size, "%02d:%02d:%02d.%d", hh, mm, ss, ss_fract);
   }
-  
-  return snprintf(buf, buf_size, "%02d:%02d.%d", mm, ss, ss_fract); 
+
+  return snprintf(buf, buf_size, "%02d:%02d.%d", mm, ss, ss_fract);
 }
 
 static int open_file(Run_timer* t)
@@ -91,27 +91,27 @@ static int open_file(Run_timer* t)
   struct tm* lt;
   char fname[PATH_MAX+1];
   uint len;
-  
+
   if (!t->file_prefix)
     return 1;
-  
+
   if ((len = strlen(t->file_prefix)) > sizeof(fname) - 3 * TIMER_DATA_PREFIX_LEN + 3)
     return 1;
-  
+
   if (!(lt = localtime(&t_now)))
     return 1;
 
   memcpy(fname,t->file_prefix,len);
-  
+
   if (!strftime(fname + len, sizeof(fname) - len, TIMER_DATA_PREFIX TIMER_DATA_FMT "." TIMER_DATA_EXT, lt))
     return 1;
-  
+
   if (!(t->fp = fopen(fname,"w")))
   {
     LOGE("Could not open file %s (%d)", fname, errno);
     return 1;
   }
-  
+
   return 0;
 }
 
@@ -120,23 +120,23 @@ int run_timer_start(Run_timer* t)
 {
   t->t_start = run_timer_now();
   t->t_pause = t->t_delay = 0;
-  
+
   if (open_file(t))
   {
     LOGE("Will not log timer data to a file");
     // OK to continue, better give user something that nothing
   }
-  
+
   return start_leg(t,0, 0.0);
 }
 
 int run_timer_resume(Run_timer* t)
 {
   ulonglong t_now = run_timer_now();
-  
+
   t->t_delay += t_now - t->t_pause;
   t->t_pause = 0;
-  
+
   if (t->fp)
   {
     fflush(t->fp);
@@ -151,26 +151,26 @@ int run_timer_info(Run_timer* t, Run_info* info)
   ulonglong t_run = run_timer_now() - t->t_delay - t->t_start;
   Run_leg* cur_leg = t->cur_leg;
   Run_split* cur_split;
-  
+
   info->t_total = t_run;
-  
+
   if (!cur_leg || !cur_leg->first_split)
   {
     info->t_total = info->t_split = info->t_leg = 0;
     info->d_last_split = info->d_last_leg = 0.0;
     return 0;
   }
-  
+
   info->t_leg = t_run - cur_leg->first_split->t;
   info->d_last_leg = cur_leg->first_split->d;
-  
+
   if (!(cur_split = cur_leg->cur_split))
-  {  
+  {
     info->t_split = 0;
     info->d_last_split = 0.0;
     return 1;
   }
-  
+
   info->t_split = t_run - cur_split->t;
   info->d_last_split = cur_split->d;
   return 0;
@@ -180,7 +180,7 @@ ulonglong run_timer_running_time(Run_timer* t)
 {
   if (t->t_pause)
     return t->t_pause - t->t_delay - t->t_start;
-  
+
   return run_timer_now() - t->t_start - t->t_delay;
 }
 
@@ -188,10 +188,10 @@ ulonglong run_timer_running_time(Run_timer* t)
 int run_timer_pause(Run_timer* t, double d)
 {
   t->t_pause = run_timer_now();
-  
+
   if (!t->fp)
     return 1;
-  
+
   t->resume_fp_pos = ftell(t->fp);
   fprintf(t->fp, "\n%llu,%g\n", run_timer_running_time(t),d);
   fflush(t->fp);
@@ -281,7 +281,7 @@ static int start_leg(Run_timer* t, ulonglong ts, double d)
   LOGE("start_leg(%llu,%.3f)",ts,d);
   if (!(cur_leg = (Run_leg*)mem_pool_alloc(&t->mem_pool,sizeof(Run_leg))))
     return 1;
-  
+
   cur_leg->cur_split = cur_leg->first_split = 0;
   t->cur_leg = cur_leg;
   cur_leg->num_splits = 0;
@@ -292,11 +292,11 @@ static int start_leg(Run_timer* t, ulonglong ts, double d)
     if (t->first_leg)
       fputc('\n',t->fp);
   }
-  
+
   if (start_split(t,ts,d))
     return 1;
-  
-  LL_APPEND(t->first_leg,t->cur_leg); 
+
+  LL_APPEND(t->first_leg,t->cur_leg);
   t->num_legs++;
   return 0;
 }
@@ -314,10 +314,10 @@ int run_timer_split(Run_timer* t, double d)
 ulonglong run_timer_now()
 {
   struct timeval t;
-  
+
   if (gettimeofday(&t,0))
     return 0;
-  
+
   return (ulonglong)t.tv_sec * 1000LL + (ulonglong)t.tv_usec/1000LL;
 }
 
@@ -335,13 +335,13 @@ char* run_timer_review_info(Run_timer* t, Run_timer_review_mode mode)
   ulonglong t_tmp;
   double d_tmp;
   uint bytes_printed;
-  
+
   if (!(buf = (char*)malloc(buf_size)))
     return 0;
-  
+
   p = buf;
   bytes_left = buf_size;
-  
+
   LL_FOREACH(t->first_leg,cur_leg)
   {
     LOGE("leg split in review:(%llu,%.3f)", cur_leg->first_split->t,cur_leg->first_split->d);
@@ -389,6 +389,14 @@ char* run_timer_review_info(Run_timer* t, Run_timer_review_mode mode)
   return buf;
 }
 
+static int rl_compare(RUN_LIST* a, RUN_LIST* b)
+{
+  if (!a) return -1;
+  if (!b) return 1;
+  //LOGE("Comparing %s and %s", a->name, b->name);
+  return strcmp(a->name, b->name);
+}
+
 char** run_timer_run_list(Run_timer* t, Mem_pool* pool,uint* num_entries)
 {
   char* dir_name = (char*)mem_pool_dup(pool,t->file_prefix,t->dir_len+1);
@@ -396,20 +404,20 @@ char** run_timer_run_list(Run_timer* t, Mem_pool* pool,uint* num_entries)
   RUN_LIST* rl_head = 0,*rl_tmp;
   DIR* d = 0;
   char** res = 0,**res_p;
-  
+
   if (!dir_name)
     return 0;
-  
+
   dir_name[t->dir_len] = 0;
-  
+
   if (!(d = opendir(dir_name)))
   {
     LOGE("Could not open directory %s (%d)", dir_name, errno);
     goto err;
   }
-  
+
  *num_entries = 0;
-  
+
   while ((d_ent = readdir(d)))
   {
     char *dot = strrchr(d_ent->d_name,'.');
@@ -433,14 +441,16 @@ char** run_timer_run_list(Run_timer* t, Mem_pool* pool,uint* num_entries)
       goto err;
     }
 
-    rl_tmp->name = (char*)(rl_tmp+1); 
+    rl_tmp->name = (char*)(rl_tmp+1);
     memcpy(rl_tmp->name,name,name_len);
     rl_tmp->name[name_len] = 0;
     rl_tmp->next = 0;
-    LOGE("rl_tmp = %p name=%p name_len = %d", rl_tmp, rl_tmp->name, name_len);
+    //LOGE("rl_tmp = %p name=%p name_len = %d", rl_tmp, rl_tmp->name, name_len);
     LL_APPEND(rl_head,rl_tmp);
     (*num_entries)++;
   }
+
+  LL_SORT(rl_head, rl_compare);
 
   if (!(res = (char**)mem_pool_alloc(pool,sizeof(char*) * (*num_entries+1))))
   {
@@ -690,10 +700,10 @@ int run_timer_init_from_workout(Run_timer* t, const char* file_prefix, const cha
   double cur_d = 0.0, cur_pow_10 = 0.1;
   int need_start_leg = 1, line_not_empty = 0;
   enum {READ_MODE_TIME, READ_MODE_DIST, READ_MODE_DIST_F} read_mode = READ_MODE_TIME;
-  
+
   if (run_timer_init(t,file_prefix))
     return 1;
-  
+
   if (len > sizeof(fname) - 3 * TIMER_DATA_PREFIX_LEN + 3)
     return 1;
 
@@ -843,7 +853,7 @@ int run_timer_init_split_arr(Run_timer* t)
     LOGE("Empty timer");
     return 1;
   }
-  
+
   if (!(t->leg_arr = (Run_leg**)mem_pool_alloc(&t->mem_pool,sizeof(Run_leg*)*t->num_legs)))
   {
     LOGE("No memory for leg array");
@@ -940,7 +950,7 @@ ulonglong run_timer_parse_time(const char* s, uint len)
         break;
     }
   }
-  
+
   if (seen_dot)
   {
     res += res_tmp * ms_mul;
@@ -1053,7 +1063,7 @@ int run_timer_add_key_to_hash(Run_timer* t, const char* key, const char* data, u
     char* new_val;
 
     LOGE("Found entry with key '%s' for key '%s'", e->key, key);
-    
+
     if (!(new_val = (char*)mem_pool_alloc(&t->mem_pool,new_val_len)))
     {
       LOGE("OOM allocating additional space for value");
