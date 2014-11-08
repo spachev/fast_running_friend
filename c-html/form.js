51d223521f788c9ecd51345fc4916ffb27393ee3
Sasha Pachev <sasha@asksasha.com>
2014-11-08 16:28:48 -0700

    Fixed bug in leg/split processing, added build-install-local script

--- c-html/form.js
+++ c-html/form.js
@@ -86,30 +86,30 @@ function sec_to_time(t_sec)
   ss = t_sec % 60;
   t_sec = ((t_sec - ss) / 60);
   ss = ss.toFixed(1);
-  
+
   if (ss < 10)
     ss = '0' + ss.toString();
   else
     ss = ss.toString();
-      
+
   mm = t_sec % 60;
-  
+
   if (mm < 10)
     mm = '0' + mm.toString();
   else
     mm = mm.toString();
-    
+
   hh = (t_sec - mm) / 60;
-  var hh_str = (hh > 0) ?  hh.toString() + ':' : ''; 
-  return  hh_str + mm + ':' + ss;  
+  var hh_str = (hh > 0) ?  hh.toString() + ':' : '';
+  return  hh_str + mm + ':' + ss;
 }
 
 function update_leg(leg_num)
 {
   var i,t_leg = 0,d_leg = 0.0;
   var in_els = document.getElementsByTagName("input");
-  var t_prefix = "t_" + leg_num;
-  var d_prefix = "d_" + leg_num;
+  var t_prefix = "t_" + leg_num + "_";
+  var d_prefix = "d_" + leg_num + "_";
   for (i = 0; i < in_els.length; i++)
   {
     if (in_els[i].name.substr(0,t_prefix.length) == t_prefix)
@@ -121,15 +121,15 @@ function update_leg(leg_num)
       d_leg += Number(in_els[i].value);
     }
   }
-  
+
   var t_span_el,d_span_el;
-  
+
   t_span_el = document.getElementById("t_" + leg_num);
   d_span_el = document.getElementById("d_" + leg_num);
-  
+
   if (!t_span_el || !d_span_el)
     return;
-  
+
   t_span_el.innerHTML = sec_to_time(t_leg);
   d_span_el.innerHTML = d_leg.toFixed(3);
 }
